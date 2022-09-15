db3462f1df20ccc2b72e9bf5a7c2b18c9d022189
ESTUDIANTES <up210713@alumnos.upa.edu.mx>
2022-09-15 09:51:18 -0500

    reposi

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,24 @@
+/*
+Date: 12/09/2022
+Author: Alan Oziel Zuñiga Garcia
+Description: Mi primer programa en C++
+Last modification:
+*/
+
+//--------------PREPROCESOR DIRECTIVES------------------
+
+
+//Input/output library between  the screen n' keyboard
+#include <iostream>
+
+
+//---------------MAIN FUNCTION------------------
+
+int main(){
+
+
+    std::cout << "Hello World! THis is my first program";
+
+    //Return 0 means that the program ends succesfully
+    return 0;
+}
